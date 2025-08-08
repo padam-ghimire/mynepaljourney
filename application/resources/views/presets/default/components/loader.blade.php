aa48ba8ab97b2f7386d9bcfe2026ddfd6518e764
Padam Ghimire <padam.ghimire@f1soft.com>
2025-08-08 16:18:42 +0545

    Preloader Changes

--- application/resources/views/presets/default/components/loader.blade.php
+++ application/resources/views/presets/default/components/loader.blade.php
@@ -1,12 +1,23 @@
     <!--==================== Preloader Start ====================-->
     <div id="preloader">
         <div id="text">
-            <p>T</p>
+            <p>M</p>
+            <p>Y</p>
+            
+            <p>J</p>
+            <p>O</p>
+            <p>U</p>
             <p>R</p>
-            <p>A</p>
-            <p>V</p>
+            <p>N</p>
             <p>E</p>
+            <p>Y</p>
+
+            <p>N</p>
+            <p>E</p>
+            <p>P</p>
+            <p>A</p>
             <p>L</p>
+
             <p class="active">A</p>
         </div>
     </div>
